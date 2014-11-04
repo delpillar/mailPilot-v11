0b401a3630fe8d51d0813fc7e867bcd30df27b9c
delpillar <knivex27@hotmail.com>
2014-11-04 17:35:11 -0500

    Change image bitmap to stars; Change image move direction

--- mailPilot-v11/Scripts/objects/island.js
+++ mailPilot-v11/Scripts/objects/island.js
@@ -1,4 +1,5 @@
-﻿/// <reference path="../managers/asset.ts" />
+/// <reference path="../managers/asset.ts" />
+/// <reference path="../managers/asset.js" />
 var objects;
 (function (objects) {
     // Island Class
@@ -6,27 +7,28 @@ var objects;
         function Island(stage, game) {
             this.stage = stage;
             this.game = game;
-            this.image = new createjs.Sprite(managers.Assets.atlas, "island");
+            //this.image = new createjs.Sprite(managers.Assets.atlas, "island");
+            this.image = new createjs.Sprite(managers.Assets.coin, "spin");
             this.width = this.image.getBounds().width;
             this.height = this.image.getBounds().height;
             this.image.regX = this.width / 2;
             this.image.regY = this.height / 2;
             this.reset();
 
-            this.dy = 5;
+            this.dx = 5;
 
             game.addChild(this.image);
         }
         Island.prototype.update = function () {
-            this.image.y += this.dy;
-            if (this.image.y > this.stage.canvas.height + this.height) {
+            this.image.x += this.dx;
+            if (this.image.x > this.stage.canvas.width + this.width) {
                 this.reset();
             }
         };
 
         Island.prototype.reset = function () {
-            this.image.x = Math.floor(Math.random() * this.stage.canvas.width);
-            this.image.y = -this.height;
+            this.image.y = Math.floor(Math.random() * this.stage.canvas.height);
+            this.image.x = -this.width;
         };
 
         Island.prototype.destroy = function () {
