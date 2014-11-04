3367bd11da008ea2a1fed377b8b7f6f2bf20e39f
delpillar <knivex27@hotmail.com>
2014-11-04 17:34:42 -0500

    Change image to lasers;  Update image move direction

--- mailPilot-v11/Scripts/objects/cloud.js
+++ mailPilot-v11/Scripts/objects/cloud.js
@@ -1,4 +1,5 @@
-﻿/// <reference path="../managers/asset.ts" />
+/// <reference path="../managers/asset.ts" />
+/// <reference path="../managers/asset.js" />
 var objects;
 (function (objects) {
     // Cloud class
@@ -6,7 +7,8 @@ var objects;
         function Cloud(stage, game) {
             this.stage = stage;
             this.game = game;
-            this.image = new createjs.Sprite(managers.Assets.atlas, "cloud");
+            //this.image = new createjs.Sprite(managers.Assets.atlas, "cloud");
+            this.image = new createjs.Bitmap(managers.Assets.loader.getResult("laser"));
             this.width = this.image.getBounds().width;
             this.height = this.image.getBounds().height;
             this.image.regX = this.width / 2;
@@ -16,18 +18,18 @@ var objects;
             game.addChild(this.image);
         }
         Cloud.prototype.update = function () {
-            this.image.y += this.dy;
+            //this.image.y += this.dy;
             this.image.x += this.dx;
-            if (this.image.y > this.stage.canvas.height + this.height) {
+            if (this.image.x > this.stage.canvas.width + this.width) {
                 this.reset();
             }
         };
 
         Cloud.prototype.reset = function () {
-            this.image.x = Math.floor(Math.random() * this.stage.canvas.width);
-            this.dy = Math.floor(Math.random() * 5 + 5);
-            this.dx = Math.floor(Math.random() * -3) + Math.floor(Math.random() * 3);
-            this.image.y = -this.height;
+            this.image.y = Math.floor(Math.random() * this.stage.canvas.height);
+            this.dx = Math.floor(Math.random() * 5 + 10);
+            //this.dy = Math.floor(Math.random() * -3) + Math.floor(Math.random() * 3);
+            this.image.x = -this.width;
         };
 
         Cloud.prototype.destroy = function () {
