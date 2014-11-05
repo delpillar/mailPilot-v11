d2ca197e3828fd6f9e2959e2ee038c4b46be6131
delpillar <knivex27@hotmail.com>
2014-11-05 04:17:48 -0500

    Change text constants

--- mailPilot-v11/Scripts/constants.js
+++ mailPilot-v11/Scripts/constants.js
@@ -6,9 +6,9 @@ var constants;
     constants.GAME_OVER_STATE = 2;
 
     // Game Constants
-    constants.CLOUD_NUM = 12;
-    constants.LABEL_FONT = "40px Consolas";
-    constants.LABEL_COLOUR = "#FFFF00";
+    constants.CLOUD_NUM = 10;
+    constants.LABEL_FONT = "20px Wallpoet";
+    constants.LABEL_COLOUR = "#FF7B16";
     constants.PLANE_LIVES = 5;
 })(constants || (constants = {}));
 //# sourceMappingURL=constants.js.map
