2e9012dad791f02356083992fdbf3cbd042f8391
delpillar <knivex27@gmail.com>
2014-11-07 12:13:32 -0500

    Add up and down animation if mouseY either -- or ++

--- mailPilot-v11/Scripts/objects/plane.js
+++ mailPilot-v11/Scripts/objects/plane.js
@@ -1,6 +1,7 @@
 /// <reference path="../managers/asset.ts" />
 /// <reference path="../managers/asset.js" />
 var objects;
+var mx, my;
 (function (objects) {
     // Plane Class
     var Plane = (function () {
@@ -11,8 +12,8 @@ var objects;
             this.up = new createjs.Sprite(managers.Assets.ship,"up");
             this.down = new createjs.Sprite(managers.Assets.ship,"down");
             this.image = this.idle;
-            this.image.x = 450;
-            this.image.y = 500;
+            this.image.x = this.stage.mouseX;
+            this.image.y = this.stage.mouseY;
             this.width = this.image.getBounds().width;
             this.height = this.image.getBounds().height;
             this.image.regX = this.width / 2;
@@ -21,8 +22,46 @@ var objects;
             this.engineSound = createjs.Sound.play('gameMusic', createjs.Sound.INTERRUPT_NONE, 0, 1500, -1, 1, 0);
         }
         Plane.prototype.update = function () {
-            this.image.y = this.stage.mouseY;
-            
+            window.setInterval(function(){
+                mx = this.stage.mouseX;    
+                my = this.stage.mouseY;
+            },30);
+                
+             if(this.image.y > my ){
+                //this.image.gotoAndStop(this.idle);
+                game.removeChild(this.image);
+                this.image = this.up;
+                this.image.regX = this.width / 2;
+                this.image.regY = this.height / 2;
+                this.image.y = this.stage.mouseY;
+                this.image.x = this.stage.mouseX;
+                this.image.gotoAndPlay(this.up);
+                game.addChild(this.image);
+            }
+            else if(this.image.y < my){
+                //this.image.gotoAndStop(this.idle);
+                //this.image.gotoAndStop(this.up);
+                game.removeChild(this.image);
+                this.image = this.down;
+                this.image.regX = this.width / 2;
+                this.image.regY = this.height / 2;
+                this.image.y = this.stage.mouseY;
+                this.image.x = this.stage.mouseX;
+                this.image.gotoAndPlay(this.down);
+                game.addChild(this.image);
+            }
+            else{
+                this.image.gotoAndStop(this.up);
+                this.image.gotoAndStop(this.down)
+                game.removeChild(this.image);
+                this.image = this.idle;
+                this.image.regX = this.width / 2;
+                this.image.regY = this.height / 2;
+                this.image.y = this.stage.mouseY;
+                this.image.x = this.stage.mouseX;
+                this.image.gotoAndPlay(this.idle);
+                game.addChild(this.image);
+            }
         };
         Plane.prototype.destroy = function () {
             this.engineSound.stop();
